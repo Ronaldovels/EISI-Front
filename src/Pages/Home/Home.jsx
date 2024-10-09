50bb59b529bbf3a1b012ec1210d7f827eef8caab
Ronaldo Veloso Filho <ronaldovf24@gmail.com>
2024-10-09 17:35:51 -0300

    Revert "adding disclaimer window"
    This reverts commit 47eb667f1e85c652ced96210b058752e65f16742.

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,12 +3,10 @@ import React from 'react';
 import './Home.css';
 import Banner from '../../componentes/HomeBanner/HomeBanner'
 import OptionContainer from '../../componentes/OptionContainer/OptionContainer';
-import WelcomeMessage from '../../componentes/Disclaimer/Disclaimer';
 
 function Home() {
   return (
     <div>
-      <WelcomeMessage/>
       <Banner/>
       <section id='guessCharacter'>
         <OptionContainer/>
